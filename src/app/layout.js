ca05a5a298495ff0474607d4bf94fb4d8a158ef0
Lopez <ialopez.contacto@gmail.com>
2025-03-07 20:47:33 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,8 +11,8 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Bulk Airdrop tool" />
-        <title>Bulk Airdrop</title>
+        <meta name="description" content="ETN Buddy Staking" />
+        <title>Buddy Staking</title>
       </head>
       <body>
         <Providers>
